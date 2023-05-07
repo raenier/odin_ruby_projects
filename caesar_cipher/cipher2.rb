21aedd8ed851836707ef1493618de50a6a22da70
raenier <raenierquiambao@gmail.com>
2023-05-07 15:54:46 +0800

    Add decrypt function for 2nd solution

--- caesar_cipher/cipher2.rb
+++ caesar_cipher/cipher2.rb
@@ -16,6 +16,24 @@ class CaesarCipher2
       end.chr
     end.join
   end
+
+  def self.decrypt(text, shift_number)
+    text.each_char.map do |char|
+      lowercase_range = 97..122
+      uppercase_range = 65..90
+      char_num = char.ord
+      case char_num
+      when lowercase_range
+        shifted = char_num - shift_number
+        (shifted < lowercase_range.first) ? lowercase_range.last - (lowercase_range.first - shifted - 1) : shifted
+      when uppercase_range
+        shifted = char_num - shift_number
+        (shifted < uppercase_range.first) ? uppercase_range.last - (uppercase_range.first - shifted - 1) : shifted
+      else char_num
+      end.chr
+    end.join
+  end
 end
 
-p CaesarCipher2.encrypt('The quicK brown foX JuMps over the Lazy dog', 99)
+p enc = CaesarCipher2.encrypt('The quicK brown foX JuMps over the Lazy dog', 5)
+p dec = CaesarCipher2.decrypt(enc, 5)
