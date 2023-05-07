77bfd250de01265f81687e992dbb0e98015418e3
raenier <raenierquiambao@gmail.com>
2023-05-07 18:34:41 +0800

    Optimize solution, use scan instead of splitting string

--- substrings/substrings.rb
+++ substrings/substrings.rb
@@ -1,11 +1,6 @@
 def substrings(sentence, dictionary)
   dictionary.reduce({}) do |result, substring|
-    next unless sentence.downcase.include?(substring.downcase)
-
-    result[substring] =
-      sentence.split(' ').reduce(0) do |ctr, word|
-        word.include?(substring) ? ctr += 1 : ctr
-      end
+    result[substring] = sentence.downcase.scan(substring.downcase).length
     result
   end
 end
