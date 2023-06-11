18871f9441f6bd93441c1896c88e3b394cdaadf3
raenier <raenierquiambao@gmail.com>
2023-06-11 19:03:27 +0800

    Add testing for decrypt

--- caesar_cipher/spec/cipher_spec.rb
+++ caesar_cipher/spec/cipher_spec.rb
@@ -35,4 +35,41 @@ describe CaesarCipher2 do
       end
     end
   end
+
+  describe '#decrypt' do
+    subject { CaesarCipher2.decrypt(word, number_shift) }
+
+    context 'Different number shift' do
+      let(:word) { 'de' }
+
+      context 'When shift number is 1' do
+        let(:number_shift) { 1 }
+        it { is_expected.to eql('cd') }
+      end
+
+      context 'When shift number is 2' do
+        let(:number_shift) { 2 }
+        it { is_expected.to eql('bc') }
+      end
+
+      context 'When shift number is 3' do
+        let(:number_shift) { 3 }
+        it { is_expected.to eql('ab') }
+      end
+    end
+
+    context 'Different given word' do
+      let(:number_shift) { 1 }
+
+      context 'Mixed cases' do
+        let(:word) { 'bCdE' }
+        it { is_expected.to eql('aBcD') }
+      end
+
+      context 'With Spaces' do
+        let(:word) { 'bc de fg' }
+        it { is_expected.to eql('ab cd ef') }
+      end
+    end
+  end
 end
